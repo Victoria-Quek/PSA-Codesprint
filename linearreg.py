425183e1c5710a4238f6be92f639b1ad9a898236
jonleewh <94356443+jonleewh@users.noreply.github.com>
2024-10-11 23:58:14 +0800

    Update linearreg.py

--- linearreg.py
+++ linearreg.py
@@ -1,13 +1,29 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import inquirer
 
 # input data
 # we need to provide some data to determine what is the ideal weights
+def query():
+    start = input("Enter your current location: ")
+    end = input("Enter your destination: ")
+    print(answers["size"])
 
-def data():
-    return
+query()
+
+# how to let them select options
+# questions = [inquirer.List('size',
+                           message="What size do you need?",
+                           choices=['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro'],
+                           ),
+             ]
+# answers = inquirer.prompt(questions)
 
 # Linear Regression
+
+# travel time: feature scaling (scale from 0 to 1)
+# distance: feature scaling (scale from 0 to 1)
+
 X = np.array([[1, 1],
               [1, 2],
               [2, 2],
